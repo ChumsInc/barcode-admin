6a02b5369274b99c5c54d8eb78722180f02e55f2
Steve Montgomery <steve@chums.com>
2023-02-27 07:56:11 -0700

    fix: Avoid multiple validation attempts when user is not logged in.

--- src/api/user.ts
+++ src/api/user.ts
@@ -6,11 +6,9 @@ export async function fetchUserValidation():Promise<UserValidationResponse> {
         const url = '/api/user/validate';
         return await fetchJSON<UserValidationResponse>(url);
     } catch(err:unknown) {
-        if (err instanceof Error) {
-            console.debug("fetchUserValidation()", err.message);
-            return Promise.reject(err);
+        return {
+            valid: false,
+            loaded: new Date().toISOString()
         }
-        console.debug("fetchUserValidation()", err);
-        return Promise.reject(new Error('Error in fetchUserValidation()'));
     }
 }
