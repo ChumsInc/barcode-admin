2fca7ddfc809c9f9a37894b75980703ef6475ffa
Steve Montgomery <steve@chums.com>
2023-05-25 15:58:23 -0600

    fix: fix logic for checked & checkedAll for item, bag, case stickers

--- src/ducks/customer/ItemStickerIcons.tsx
+++ src/ducks/customer/ItemStickerIcons.tsx
@@ -6,26 +6,27 @@ import {selectCurrentCustomer} from "./selectors";
 
 const ItemStickerIcons = ({item}: { item?: BarcodeItem }) => {
     const customer = useSelector(selectCurrentCustomer);
-    const {itemSticker, bagSticker, caseSticker} = item ?? {};
+    const {itemSticker = false, bagSticker = false, caseSticker = false} = item ?? {};
+    const {itemStickerAll = false, bagStickerAll = false, caseStickerAll = false} = customer ?? {};
     return (
         <div>
             <span className={classNames('me-1', {
-                'bi-1-square': !itemSticker,
-                'bi-1-square-fill': itemSticker || customer?.itemStickerAll,
-                'text-black-50': !itemSticker,
-                'text-primary': itemSticker || customer?.itemStickerAll
+                'bi-1-square': !(itemSticker || itemStickerAll),
+                'bi-1-square-fill': itemSticker || itemStickerAll,
+                'text-black-50': !(itemSticker || itemStickerAll),
+                'text-primary': itemSticker || itemStickerAll
             })}/>
             <span className={classNames('me-1', {
-                'bi-bag': !bagSticker,
-                'bi-bag-fill': bagSticker,
-                'text-black-50': !bagSticker || customer?.bagStickerAll,
-                'text-primary': bagSticker || customer?.bagStickerAll
+                'bi-bag': !(bagSticker || itemStickerAll),
+                'bi-bag-fill': bagSticker || bagStickerAll,
+                'text-black-50': !(bagSticker || bagStickerAll),
+                'text-primary': bagSticker || bagStickerAll
             })}/>
             <span className={classNames('me-1', {
-                'bi-box': !caseSticker,
-                'bi-box-fill': caseSticker || customer?.caseStickerAll,
-                'text-black-50': !caseSticker,
-                'text-primary': caseSticker || customer?.caseStickerAll
+                'bi-box': !(caseSticker || caseStickerAll),
+                'bi-box-fill': caseSticker || caseStickerAll,
+                'text-black-50': !(caseSticker || caseStickerAll),
+                'text-primary': caseSticker || caseStickerAll
             })}/>
         </div>
     )
